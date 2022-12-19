1ea7e75e32a4456ab2f05839893b1d8240ebfcc7
Joydron <ishimianderson@gmail.com>
2022-12-19 23:20:13 +0500

    repetitive loops removed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -24,69 +24,57 @@ namespace HtmlAgilityPack
                               "http://161.8.189.114/basic/antennas.psp",
                               "http://10.8.65.150/basic/antennas.psp"
             };
-            for (int e = 0; e < 15; e++)
-            {
-                HtmlWeb web = new HtmlWeb();
-                HtmlDocument document = web.Load(webs[e]);
-                Console.WriteLine(webs[e]);
-                Console.WriteLine();
-                string[] cp = { "//input[@id='conducted_power1']",
+
+            string[] cp = { "//input[@id='conducted_power1']",
                             "//input[@id='conducted_power2']",
                             "//input[@id='conducted_power3']",
                             "//input[@id='conducted_power4']"};
 
-                string[] attenuation = {"//input[@id='attenuation1']",
+            string[] attenuation = {"//input[@id='attenuation1']",
                                     "//input[@id='attenuation2']",
                                     "//input[@id='attenuation3']",
                                     "//input[@id='attenuation3']"
                                     };
 
-                string[] cableLoss = {"//input[@id='cable_loss1']",
+            string[] cableLoss = {"//input[@id='cable_loss1']",
                                     "//input[@id='cable_loss2']",
                                     "//input[@id='cable_loss3']",
                                     "//input[@id='cable_loss4']"
                                     };
 
-                string[] gain = {"//input[@id='gain1']",
+            string[] gain = {"//input[@id='gain1']",
                                     "//input[@id='gain2']",
                                     "//input[@id='gain3']",
                                     "//input[@id='gain4']"
                                     };
 
-                Console.WriteLine("Name \t Antenna 1 \t Antenna 2 \t Antenna 3 \t Antenna 4\t");
+            Dictionary<string, string[]> loops = new Dictionary<string, string[]>();
+            loops.Add("Power", cp);
+            loops.Add("Att", attenuation);
+            loops.Add("Cable", cableLoss);
+            loops.Add("Gain", gain);
 
-                Console.Write("Conducted \nPower \t \t");
-                for (int i = 0; i < 4; i++)
-                {
-                    var value = document.DocumentNode.SelectSingleNode(cp[i]);
-                    var valueFormId = value.GetAttributeValue("value", "");
-                    Console.Write(valueFormId + "\t" + "\t");
-                }
-
-                Console.Write("\nAttenuation \t");
-                for (int i = 0; i < 4; i++)
-                {
-                    var value = document.DocumentNode.SelectSingleNode(attenuation[i]);
-                    var valueFormId = value.GetAttributeValue("value", "");
-                    Console.Write(valueFormId + "\t" + "\t");
-                }
-
-                Console.Write("\nCable Loss\t");
-                for (int i = 0; i < 4; i++)
-                {
-                    var value = document.DocumentNode.SelectSingleNode(cableLoss[i]);
-                    var valueFormId = value.GetAttributeValue("value", "");
-                    Console.Write(valueFormId + "\t" + "\t");
-                }
-
-                Console.Write("\nGain\t\t");
-                for (int i = 0; i < 4; i++)
-                {
-                    var value = document.DocumentNode.SelectSingleNode(gain[i]);
-                    var valueFormId = value.GetAttributeValue("value", "");
-                    Console.Write(valueFormId + "\t" + "\t");
-                }
+            for (int e = 0; e < webs.Length; e++)
+            {
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument document = web.Load(webs[e]);
+                Console.WriteLine(webs[e]);
                 Console.WriteLine();
+
+                Console.WriteLine("Name \t Antenna 1 \t Antenna 2 \t Antenna 3 \t Antenna 4\t");
+               
+                foreach (var key in loops.Keys)
+                {
+                    Console.Write(key + "\t" + "\t");
+                    for (int j = 0; j < 4; j++)
+                    {
+                        var value = document.DocumentNode.SelectSingleNode(loops[key][j]);
+                        var valueFormId = value.GetAttributeValue("value", "");
+                        Console.Write(valueFormId + "\t" + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                
             }
 
         }
